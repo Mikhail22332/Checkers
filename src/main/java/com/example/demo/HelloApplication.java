13bfdb28a21ecd30f8d9fb9690ad8e588d10e3b5
Mikhail22332 <mixfilatov@gmail.com>
2022-12-14 23:55:47 +0100

    Added resizable checkers board

--- src/main/java/com/example/demo/HelloApplication.java
+++ src/main/java/com/example/demo/HelloApplication.java
@@ -1,8 +1,7 @@
 package com.example.demo;
 
+import com.Client.Board;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,10 +9,8 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
+        Board board = new Board();
+        board.boardCreation(stage);
         stage.show();
     }
 
